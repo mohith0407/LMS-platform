12c919b2be9def45b77785f9a9d2dfda01f3fde8
Mohith <113021993+mohith0407@users.noreply.github.com>
2025-01-31 15:04:50 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -19,7 +19,7 @@ app.use(cookieParser());
 app.use(morgan('dev'));
 app.use(
     cors({
-      origin: "http://localhost:5173/",
+      origin: "https://lms-fun.onrender.com",
       credentials: true,
     })
   );
